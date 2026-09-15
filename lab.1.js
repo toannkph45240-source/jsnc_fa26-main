bb9ee28b61ebb7768fc83c7eb7eb402fef09a919
Toan <email-cua-ban@example.com>
2026-09-15 16:00:23 +0700

    toannk

---
+++ lab.1.js
@@ -0,0 +1,8 @@
+console.log ("đây là lab 1");
+
+let name = "nktoan";
+name = "nguyen khanh toan";
+const age = 22;
+
+console.log ("ten tôi là: " , name);
+console.log ("tuổi của tôi là: " , age); 
